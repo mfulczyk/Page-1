443ea149b77922f9e0c91a1f13eedcbd0044b339
mfulczyk <fulczyk.michal@gmail.com>
2020-10-27 08:35:34 +0100

    1 commit /cleanup

---
+++ portfolio-page/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>gówno</div>;
+}
+
+export default App;
---
+++ portfolio-page/src/components/testComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const testComp = props => {
+    return (
+        <div>
+            góóównooo
+        </div>
+    )
+}
+
+
+export default testComp
